ee7769c39b58274306375471adffa8ade991cfef
KaranChoudhary <kchoudhary510199@gmail.com>
2022-01-22 14:47:39 +0530

    get_data added

---
+++ src/get_data.py
@@ -0,0 +1,27 @@
+## read params
+# process the parameters
+# return dataframe
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path, 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    data_path = config["data_source"]["s3_source"]
+    df = pd.read_csv(data_path,sep=",",encoding='utf-8')
+    return df
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args = args.parse_args()
+    data = get_data(config_path=parsed_args.config)
