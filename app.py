225e5e214b3c1e10c9589f9b65e47f6ff4d914b0
KaranChoudhary <kchoudhary510199@gmail.com>
2022-01-23 14:39:41 +0530

    app created and tested

--- app.py
+++ app.py
@@ -15,12 +15,51 @@ print(template_dir)
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-@app.route('/',methods=["GET","POST"])
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(os.path.join(model_dir_path))
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction[0]
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response" : response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error" : str(e)}
+        return error
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        pass
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float,data))]
+                response = predict(data)
+                return render_template("index.html",response=response)
+
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later."}
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
